8a61c0bb2158d136e79205bf516bdea54e6dd55d
Redjaw <davide.vernassa@gmail.com>
2017-07-05 17:44:35 +0200

    added target and threshold value numbers

--- docs/app.js
+++ docs/app.js
@@ -1,26 +1,58 @@
 'use strict';
 var myApp = angular.module('demoApp', ['r_linear_gauge']);
-myApp.config(['$compileProvider', function ($compileProvider) {
-  $compileProvider.debugInfoEnabled(false);
+myApp.config(['$compileProvider', function($compileProvider) {
+    $compileProvider.debugInfoEnabled(false);
 }]);
-myApp.controller('mainCtrl', function($scope,$interval) {
-    $scope.demoModel=24;
+myApp.controller('mainCtrl', function($scope, $interval) {
+    $scope.demoModel = 24;
     $scope.demoThresholds = [{
-        'color':'#039BE5',
-        'min':0,
-        'max':60
-    },{
-        'color':'#0288D1',
-        'min':60,
-        'max':80
-    },{
-        'color':'#0277BD',
-        'min':80,
-        'max':180
+        'color': '#039BE5',
+        'min': 0,
+        'max': 60
+    }, {
+        'color': '#0288D1',
+        'min': 60,
+        'max': 80
+    }, {
+        'color': '#0277BD',
+        'min': 80,
+        'max': 180
+    }];
+
+    $scope.demo2Thresholds = [{
+        'color': '#FFB74D',
+        'min': 0,
+        'max': 60
+    }, {
+        'color': '#FFA726',
+        'min': 60,
+        'max': 80
+    }, {
+        'color': '#FF9800',
+        'min': 80,
+        'max': 180
+    }, {
+        'color': '#FB8C00',
+        'min': 180,
+        'max': 210
+    }];
+
+    $scope.demo3Thresholds = [{
+        'color': '#8BC34A',
+        'min': 0,
+        'max': 60
+    }, {
+        'color': '#FFC107',
+        'min': 60,
+        'max': 120
+    }, {
+        'color': '#F44336',
+        'min': 120,
+        'max': 180
     }]
 
-    $interval(function(){
-        $scope.demoModel = Math.random()*180;
+    $interval(function() {
+        $scope.demoModel = Math.random() * 180;
     }, 3000);
 
 });
--- src/r-linear-gauge.js
+++ src/r-linear-gauge.js
@@ -21,58 +21,59 @@ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.*/
 'use strict';
-(function(){
+(function() {
 
     angular.module('r_linear_gauge', [])
-    .directive('rLinearGauge', function($q) {
-        return {
-            scope: {
-                ngModel: "=",
-                thresholds: "=",
-                theme: "@?",
-                tickNumber: "@?"
-            },
-            templateUrl: '../src/templates/rLinearGauge.tpl.html',
-            replace: true,
-            link: function(scope,elem,attr){
-                scope.maximumValues = [],
-                scope.minimumValues = [];
-                scope.totalTicks = scope.tickNumber!=undefined?parseInt(scope.tickNumber):10;
+        .directive('rLinearGauge', function($q) {
+            return {
+                scope: {
+                    ngModel: "=",
+                    target: "=",
+                    thresholds: "=",
+                    theme: "@?",
+                    tickNumber: "@?"
+                },
+                templateUrl: '../src/templates/rLinearGauge.tpl.html',
+                replace: true,
+                link: function(scope, elem, attr) {
+                    scope.maximumValues = [],
+                        scope.minimumValues = [];
+                    scope.totalTicks = scope.tickNumber != undefined ? parseInt(scope.tickNumber) : 10;
 
-                scope.getMinorTicks = function(num) {
-                    return new Array(num);  
+                    scope.getMinorTicks = function(num) {
+                        return new Array(num);
+                    }
+
+                    scope.getMinMax = function() {
+                        var deferred = $q.defer();
+                        var promise = deferred.promise;
+                        promise.then(function() {
+                            angular.forEach(scope.thresholds, function(value, key) {
+                                scope.maximumValues.push(value.max);
+                                scope.minimumValues.push(value.min);
+                            })
+                        }).then(function() {
+                            scope.min = Math.min.apply(Math, scope.minimumValues);
+                            scope.max = Math.max.apply(Math, scope.maximumValues);
+                        });
+                        deferred.resolve();
+                    };
+
+                    scope.getMinMax();
+
+                    scope.getTickOffset = function(element) {
+                        return ((element + 1) / scope.totalTicks) * 100 + '%';
+                    };
+
+                    scope.normalize = function(value) {
+                        return (value / scope.max) * 100 + '%';
+                    };
+
+                    scope.normalizeRange = function(value) {
+                        return ((value.max - value.min) / scope.max) * 100 + '%';
+                    };
                 }
-
-                scope.getMinMax = function () {
-                    var deferred = $q.defer();
-                    var promise = deferred.promise;
-                    promise.then(function () {
-                        angular.forEach(scope.thresholds, function(value, key) {
-                            scope.maximumValues.push(value.max);
-                            scope.minimumValues.push(value.min);
-                        })
-                    }).then(function () {
-                        scope.min = Math.min.apply(Math,scope.minimumValues);
-                        scope.max = Math.max.apply(Math,scope.maximumValues);
-                    });
-                    deferred.resolve();
-                };
-
-                scope.getMinMax();
-
-                scope.getTickOffset = function(element){
-                    return ((element+1)/scope.totalTicks)*100+'%';
-                };
-
-                scope.normalize = function(value){
-                    return (value/scope.max)*100;
-                };
-
-                scope.normalizeRange = function(value){
-                    return ((value.max-value.min)/scope.max)*100;
-                };
-            }
-        };
-    });
+            };
+        });
 
 })();
